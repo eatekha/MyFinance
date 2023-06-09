ff3c5700349283acbcf9eb26ef0e02057adcf272
eatekha <atekhaeseosa@gmail.com>
2023-06-09 19:21:19 -0400

    Seperation of Concerns

--- server.js
+++ server.js
@@ -4,35 +4,21 @@ const { json } = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
-const { dirname, join } = require('path');
-const path = require('path');
 dotenv.config();
 
 
-//express declared using constant app
+//Declarations
 const app = express();
 const PORT = process.env.PORT || 4000;
-const corsOptions = {credentials:true, origin: process.env.URL || '*'};
-
-//
+const corsOptions = { credentials: true, origin: process.env.URL || '*' };
 app.use(cors(corsOptions));
 app.use(json());
 app.use(cookieParser());
 
-//
-app.use('/', express.static(join(__dirname, 'public')));
-app.listen(PORT, ()=>console.log(`Server is listening on ${PORT}`));
 
-//Main Page
-app.get('/', (req, res) => {
-  res.send('Handling Main Page');
-});
-
-
-//Connecting Database
+//Connecting to Database
 const { Pool } = require("pg");
 const pass = require('./password');
-
 const pool = new Pool({
   user: "postgres",
   password: pass,
@@ -41,19 +27,11 @@ const pool = new Pool({
   database: "userDatabase"
 });
 
-app.post('/register', (req, res) => {
-  const { user_name, user_password } = req.body;
 
-  // Insert user data into the database
-  pool.query('INSERT INTO usertable (user_name, user_password) VALUES ($1, $2)', [user_name, user_password])
-    .then(() => {
-      res.status(201).json({ message: 'User registered successfully' });
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).json({ error: 'An error occurred' });
-    });
-});
+//Register Route 
+const registerUser = require('./routes/registerRoute');
+app.post('/register', registerUser);
 
 
-//Register Page
+//Login Route
+app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
