8c400e8c4182e0629a7f90b5dde7184b7eac9119
eatekha <atekhaeseosa@gmail.com>
2023-06-08 20:13:22 -0400

    Connection between server and database

--- server.js
+++ server.js
@@ -27,3 +27,33 @@ app.listen(PORT, ()=>console.log(`Server is listening on ${PORT}`));
 app.get('/', (req, res) => {
   res.send('Handling Main Page');
 });
+
+
+//Connecting Database
+const { Pool } = require("pg");
+const pass = require('./password');
+
+const pool = new Pool({
+  user: "postgres",
+  password: pass,
+  host: "localhost",
+  port: 5432,
+  database: "userDatabase"
+});
+
+app.post('/register', (req, res) => {
+  const { user_name, user_password } = req.body;
+
+  // Insert user data into the database
+  pool.query('INSERT INTO usertable (user_name, user_password) VALUES ($1, $2)', [user_name, user_password])
+    .then(() => {
+      res.status(201).json({ message: 'User registered successfully' });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ error: 'An error occurred' });
+    });
+});
+
+
+//Register Page
