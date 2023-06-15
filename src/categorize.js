8cdf05db22d3f7fad26a21b8405835f32a3c9bae
eatekha <atekhaeseosa@gmail.com>
2023-06-14 20:18:02 -0400

    Dataset to be used to categorize transactions

--- src/categorize.js
+++ src/categorize.js
@@ -1,22 +1,64 @@
-const filePath = "pcbanking (3) - Copy.csv";
+const filePath = "pcbanking (3).csv";
 const fs = require("fs");
+var dataSet = require('./dataset');
 
-
+//Read in File
 fs.readFile(filePath, "utf-8", function (err, contents) {
   if (err) {
-    throw (err);
+    throw err;
   }
 
+  //Variables
   const rows = contents.split("\n");
-
-  for (let i = 1; i < rows.length-1; i++) {
+  const transactions = [];
+  const amounts = [];
+  
+  //Extracting Data
+  for (let i = 1; i < rows.length - 1; i++) {
     const columns = rows[i].split(",");
 
-    if (columns.length >= 2) {
-      const description = columns[1]; // Assuming the description column is the second column
-      console.log(description);
-    } else {
-      throw new Error("Invalid CSV file structure. Description column not found in row.");
+    if (columns.length >= 3) {
+      const transaction = columns[1]; // Assuming the description column is the second column
+      const amount = parseFloat(columns[2].substring(1)); // Assuming the amount column is the third column
+
+      transactions.push(transaction);
+      amounts.push(amount);
+        } else {
+      throw new Error("Invalid CSV file structure. Description or amount column not found in row.");
     }
   }
+
+
+/*
+Categories = 
+{   - Food & Dining
+    - Entertainment
+    - Groceries
+    - Health, Fitness Personal Care
+    - Shopping
+    - Transfer
+    - Transportation
+    - Travel
+    - Utilities
+    - Other
+  }
+    */
+
+const result = transactions.map((transaction, index) => {
+  return {
+    transaction: transaction,
+    amount: amounts[index]
+  };
 });
+
+console.log(dataSet);
+});
+
+/*
+This Machine Learning Model employs the Naive Bayes Algorithm, to categorize each transaction into their 
+respective category"
+*/
+
+
+
+
---
+++ src/dataset.js
@@ -0,0 +1,172 @@
+dataSet = [
+    {
+      "transaction": "TIM HORTONS #0770        CAMBRIDGE    ON  (APPLE PAY) ",
+      "amount": 4.16,
+      "category": "Food & Dining",
+      "keyword": "Tim Hortons"
+    },
+    {
+      "transaction": "EASY PAY  ADJ            5507159 ON  (INTERAC) ",
+      "amount": 24.5,
+      "category": "Transfer",
+      "keyword": "Easy Pay"
+    },
+    {
+      "transaction": "AMAZON.CA               5523550 ON  (INTERAC) ",
+      "amount": 15.37,
+      "category": "Shopping",
+      "keyword": "Amazon"
+    },
+    {
+      "transaction": "METRO #041              CAMBRIDGE    ON  (APPLE PAY) ",
+      "amount": 9.48,
+      "category": "Groceries",
+      "keyword": "Metro"
+    },
+    {
+      "transaction": " AMZ*Brands Inn           WWW.AMAZON.CAON ",
+      "amount": 5.02,
+      "category": "Shopping",
+      "keyword": "Amazon"
+    },
+    {
+      "transaction": "FOOD BASICS  633         CAMBRIDGE    ON  (APPLE PAY) ",
+      "amount": 1.12,
+      "category": "Groceries",
+      "keyword": "Food Basics"
+    },
+    {
+      "transaction": "POPEYES 11159            CAMBRIDGE    ON  (APPLE PAY) ",
+      "amount": 9.03,
+      "category": "Food & Dining",
+      "keyword": "Popeyes"
+    },
+    {
+      "transaction": "POPEYES 11159            CAMBRIDGE    ON  (APPLE PAY) ",
+      "amount": 9.03,
+      "category": "Food & Dining",
+      "keyword": "Popeyes"
+    },
+    {
+      "transaction": "MANDARIN KITCHENER       KITCHENER    ON  (APPLE PAY) ",
+      "amount": 42.93,
+      "category": "Food & Dining",
+      "keyword": "Mandarin"
+    },
+    {
+      "transaction": "FIDO Mobile ******7202   888-481-3436 ON ",
+      "amount": 65.54,
+      "category": "Utilities",
+      "keyword": "Fido Mobile"
+    },
+    {
+      "transaction": "MCDONALD'S #25001        CAMBRIDGE    ON  (APPLE PAY) ",
+      "amount": 6.31,
+      "category": "Food & Dining",
+      "keyword": "McDonald's"
+    },
+    {
+      "transaction": "WAL-MART SUPERCENTER#3152CAMBRIDGE    ON  (APPLE PAY) ",
+      "amount": 32.42,
+      "category": "Shopping",
+      "keyword": "Walmart"
+    },
+    {
+      "transaction": "TIM HORTONS #2245        CAMBRIDGE    ON  (APPLE PAY) ",
+      "amount": 10.26,
+      "category": "Food & Dining",
+      "keyword": "Tim Hortons"
+    },
+    {
+      "transaction": "CDN TIRE STORE #00168    CAMBRIDGE    ON  (APPLE PAY) ",
+      "amount": 101.69,
+      "category": "Shopping",
+      "keyword": "Canadian Tire"
+    },
+    {
+      "transaction": "          FROM - *****04* 00450238066",
+      "amount": 145,
+      "category": "Transfer",
+      "keyword": "From"
+    },
+    {
+      "transaction": "GATEWAY NEWSTAND #00161  CAMBRIDGE    ON  (APPLE PAY) ",
+      "amount": 2.17,
+      "category": "Shopping",
+      "keyword": "Gateway Newstand"
+    },
+    {
+      "transaction": "DOULOS PENTECOSTAL TABERN KITCHENER    ON  (APPLE PAY) ",
+      "amount": 100,
+      "category": "Other",
+      "keyword": "Doulos Pentecostal Tabern"
+    },
+    {
+      "transaction": "SHOES AND THINGS         CAMBRIDGE    ON  (APPLE PAY) ",
+      "amount": 41.17,
+      "category": "Shopping",
+      "keyword": "Shoes and Things"
+    },
+    {
+      "transaction": "TRANSFER FROM CAD      XX7461 ",
+      "amount": 38,
+      "category": "Transfer",
+      "keyword": "Transfer from CAD"
+    },
+    {
+      "transaction": "KFC/TACO BELL/PZA HUT#358 CAMBRIDGE    ON  (APPLE PAY) ",
+      "amount": 4.36,
+      "category": "Food & Dining",
+      "keyword": "KFC/Taco Bell/Pizza Hut"
+    },
+    {
+      "transaction": "DOULOS PENTECOSTAL TABERN KITCHENER    ON  (APPLE PAY) ",
+      "amount": 20,
+      "category": "Other",
+      "keyword": "Doulos Pentecostal Tabern"
+    },
+    {
+      "transaction": "SIMON CHAN DVM           CAMBRIDGE    ON  (APPLE PAY) ",
+      "amount": 193.5,
+      "category": "Health, Fitness Personal Care",
+      "keyword": "Simon Chan DVM"
+    },
+    {
+      "transaction": "LIQUOR 4 U               CAMBRIDGE    ON  (APPLE PAY) ",
+      "amount": 55.49,
+      "category": "Food & Dining",
+      "keyword": "Liquor 4 U"
+    },
+    {
+      "transaction": "MCDONALD'S #11019        CAMBRIDGE    ON  (APPLE PAY) ",
+      "amount": 6.76,
+      "category": "Food & Dining",
+      "keyword": "McDonald's"
+    },
+    {
+      "transaction": "Amazon.ca Prime Member   amazon.ca/priBC ",
+      "amount": 7.5,
+      "category": "Shopping",
+      "keyword": "Amazon"
+    },
+    {
+      "transaction": "MCDONALD'S #40289       KITCHENER    ON  (APPLE PAY) ",
+      "amount": 6.76,
+      "category": "Food & Dining",
+      "keyword": "McDonald's"
+    },
+    {
+      "transaction": "ZEHRS S CAMBRIDGE #576   CAMBRIDGE    ON  (APPLE PAY) ",
+      "amount": 4.41,
+      "category": "Groceries",
+      "keyword": "Zehrs"
+    },
+    {
+      "transaction": "WAL-MART SUPERCENTER#3152CAMBRIDGE    ON  (APPLE PAY) ",
+      "amount": 57.23,
+      "category": "Shopping",
+      "keyword": "Walmart"
+    }
+  ]
+
+module.exports = dataSet;
