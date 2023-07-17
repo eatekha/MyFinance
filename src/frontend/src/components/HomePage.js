84f0f5da008a17f0b25cba571073baa6a6934370
eatekha <atekhaeseosa@gmail.com>
2023-07-17 18:23:40 -0400

    Added Links to Homepage

--- src/frontend/src/components/HomePage.js
+++ src/frontend/src/components/HomePage.js
@@ -4,8 +4,16 @@ const HomePage = () => {
   return (
     <div>
       <h1>Welcome to MyFinance!!</h1>
-      <p>This is the boilerplate home page for your React application.</p>
-      {/* Add more content or components as needed */}
+      <p>This is the boilerplate home page for your React application.</p>  
+      {/*ul = unordered list
+          li = each element in unorderend link
+          a = how we put link in*/}
+          
+      <h2>Component Links</h2>
+      <ul>
+       <li><a href="http://localhost:3000/login">Login</a></li>
+       <li><a href="http://localhost:3000/register">Register</a></li>
+      </ul> 
     </div>
   );
 };
