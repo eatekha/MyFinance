09a0e2c89860ab87496c14a2b1e70e05ab9bb558
eatekha <atekhaeseosa@gmail.com>
2023-09-30 18:49:51 -0400

    Error with summaryTransactionsAPI

--- src/api/summaryTransactionsAPI.js
+++ src/api/summaryTransactionsAPI.js
@@ -1,5 +1,5 @@
 /**
- * This function takes in the user_name and returns an array of f
+ * This function takes in the user_id and returns an array of f
  * Total User Earnings
  * Total User Expenses
  * Total Transactions
@@ -17,13 +17,12 @@ const router = express.Router();
 
 module.exports = (pool) => {
     router.post('/', async (req, res) => {
-        const { user_name, month} = req.body;
+        const { user_id, month} = req.body;
 
         try {
-            const userID = await getUserID(user_name);
-            const totalEarnings = await getTotalEarnings(userID, month);
-            const totalExpenses = await getTotalExpenses(userID, month);
-            const transactionNumber = await getTransactionNumber(userID, month);
+            const totalEarnings = await getTotalEarnings(user_id, month);
+            const totalExpenses = await getTotalExpenses(user_id, month);
+            const transactionNumber = await getTransactionNumber(user_id, month);
 
             if (totalEarnings !== 0) {return res.status(200).json(
                 
@@ -43,29 +42,12 @@ module.exports = (pool) => {
             res.status(500).json({ error: 'Internal server error' });
           }
         });
-
-    async function getUserID(username) {
-        const query = 'SELECT user_id FROM usertable WHERE user_name = $1;';
-        const values = [username];
-        
-        try {
-          const result = await pool.query(query, values);
-          if (result.rows.length > 0) {
-            return result.rows[0].user_id;
-          } else {
-            return null; // Return null if no matching user found
-          }
-        } catch (error) {
-          console.error("Error fetching user ID:", error);
-          throw error;
-        }
-      }
       
 
 
       /**
        * 
-       * @param {*} username 
+       * @param {*} userid 
        * @returns 
        */
 
